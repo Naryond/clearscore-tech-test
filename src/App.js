7cc1d4d75eaf634b871cb44e8e86e26335c48b85
Naryond <giuggey85@gmail.com>
2022-05-12 00:42:09 +0100

    Version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import blockNote from "./blockNotes.jpg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="boxes">
+      <header className="header">
+        <h1 className="h1">Our Ideas</h1>
       </header>
+      <div className="externalBox">
+        <div className="singleBox">
+          <img src={blockNote} alt="notes" />
+        </div>
+        <div className="singleBox">
+          <img src={blockNote} alt="notes" />
+        </div>
+      </div>
     </div>
   );
 }
