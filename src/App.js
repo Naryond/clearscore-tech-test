e219cbe506a34a450f8432c0c9f646cd3bbe7f71
Naryond <giuggey85@gmail.com>
2022-05-12 01:54:47 +0100

    Version 1.1

--- src/App.js
+++ src/App.js
@@ -5,15 +5,27 @@ import Idea from "./Idea";
 function App() {
   // const [state, setState] = useState();
 
-  const list = [{ elm: 0 }, { elm: 1 }];
+  let list = [{ elm: 0 }, { elm: 1 }];
+
+  const addOne = () => {
+    return list.push({ elm: list.length + 1 });
+  };
+
+  const removeOne = () => {
+    return list.pop();
+  };
 
   return (
     <div className="boxes">
       <header className="header">
         <h1 className="h1">Our Ideas</h1>
       </header>
-      <button className="buttonAdd">Add</button>
-      <button className="buttonRemove">Remove</button>
+      <button className="buttonAdd" type="submit" onClick={addOne}>
+        Add
+      </button>
+      <button className="buttonRemove" type="submit" onClick={removeOne}>
+        Remove
+      </button>
       <div className="externalBox">
         {list.map((x) => (
           <ul key={x.elm}>{<Idea />}</ul>
