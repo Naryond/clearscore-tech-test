d94c174f060859564370ef78ed6b6a0f5dfc245f
Naryond <giuggey85@gmail.com>
2022-05-14 17:40:52 +0100

    V.1.13 r
    Version reviewed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,52 @@ import "./App.css";
 import "./Button.css";
 import Idea from "./Idea";
 
+// whenever updating state
+// 1 - copy current state
+// 2 - manipulate the copy of state (not the actual state)
+// 3 - reset state with updated copy
+
+const pets = ['dog','cat', 'fish'];
+
+console.log(pets.pop());
+
+const card = {
+  title: 'goodbye',
+  description: 'Today is a nice day for a walk',
+  date: new Date(),
+  id: 1
+}
+
+const card2 = {
+  title: 'hello',
+  description: 'Today is a nice day for a walk',
+  date: new Date(),
+  id:2
+}
+
 function App() {
-  const [list, setList] = useState([{ a: 1 }]);
+  const [list, setList] = useState([card]);
 
   const addOne = () => {
-    setList(() => [...list, list.push()]);
+    // HOW DO WE GET TITLE HERE?
+    const newCard = {
+      title: 'hello',
+      description: 'Today is a nice day for a walk',
+      date: new Date(),
+      id: 3
+    };
+
+    setList([...list, newCard])
+    // setList((prevState) => [...list, 1]);
   };
 
-  const removeOne = () => {
-    setList(() => [list.pop()]);
+  const removeOne = (card) => {
+    console.log(card)
+    // replace list with an array with the last item currently in state
+
+    // [{ a: 1 }, { b: 2 }, { c: 3 }, { d: 4 }]
+
+    // setList(() => [list.pop()]);
   };
 
   return (
@@ -19,13 +56,16 @@ function App() {
       <header className="header">
         <h1 className="h1">Our Ideas</h1>
       </header>
+      MOVE THIS INTO IDEA
       <button className="button" type="submit" onClick={addOne}>
         Add New Tile
       </button>
       <div className="externalBox">
-        {list.map((i) => (
-          <ul key={i}>{<Idea cancel={removeOne} />}</ul>
-        ))}
+        {list.map((item) => {
+          return (
+          <ul key={item}><Idea card={item} cancel={removeOne} /></ul>
+        )
+        })}
       </div>
     </div>
   );
--- src/Idea.js
+++ src/Idea.js
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import blockNote from "./blockNotes.jpg";
 import "./Idea.css";
 import './Button.css'
 
-const Idea = (props) => {
+const Idea = ({ cancel, card}) => {
+  const [title, setTitle] = useState('');
+
   return (
     <div className="singleBox">
       <img src={blockNote} alt="notes" />
-      <input className="inputTitle" type="text" defaultValue="New Idea" />
+      <input className="inputTitle" type="text" defaultValue="New Idea"  />
       <input className="inputIdea" type="text" />
-      <button className='button regolation' onClick={props.cancel}>Remove</button>
+      <p>{card.title}</p>
+      <button className='button regolation' onClick={() => cancel(card)}>Remove</button>
     </div>
   );
 };
